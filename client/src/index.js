7d5c181f6cbe062cda4ba8acfa0e81bab8f61c00
Claudio Scheer <claudioscheer@protonmail.com>
2018-05-17 10:17:58 -0300

    Project renamed

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import './assets/css/index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
